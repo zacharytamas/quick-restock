a0cca732f70664f74046f707eba94cf04fe5df6d
Zachary Jones <zachary@jones.cx>
2024-04-22 08:47:26 -0400

    Refactor to make bar code matches more dynamic
    They now have more information than just a URL. The new struct contains
    information about the product which can be used to derive the URL.

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 This is a tiny web server which I wrote while exploring using Go for personal projects. I use Go at Google for my day job but I've tended to use TypeScript for personal projects for years now.
 
 All this application does is redirect you to a configured URL based on the path you request. This is used as part of an automation I have setup on my phone using Apple Shortcuts. Triggering the shortcut immediately gives me a camera view to scan barcodes on items, then by communicating with this server I am deep-linked into the corresponding app for repurchasing the item. It's just a quick way to restock items I've run out of.
+
+## Usage
+
+The mappings between bar codes and URLS are expected to be stored in a Yaml file called `data.yml` in the root of the project. An example is provided in `data.yaml.example`.
+
+To run the server, simply run `go run main.go`. The server will be available at `http://localhost:3500`, by default.
--- main.go
+++ main.go
@@ -2,38 +2,70 @@ package main
 
 import (
 	_ "embed"
-	"log"
+	"fmt"
+	"os"
 
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/log"
+	"github.com/gofiber/fiber/v3/middleware/logger"
 	"gopkg.in/yaml.v3"
 )
 
-type BarcodeConfig struct {
-	Codes map[string]string `yaml:"codes"`
+// BarcodeMatch contains information about a product associated with a bar code.
+type BarcodeMatch struct {
+	Barcode string `yaml:"barcode"`
+	Name    string `yaml:"name"`
+	Kind    string `yaml:"kind"`
+	Id      string `yaml:"id"`
 }
+
+// BarcodeConfig is the configuration for mapping bar codes to URLs.
+type BarcodeConfig map[string]BarcodeMatch
 
 //go:embed data.yml
 var barcodeConfig string
-var barCodeMap map[string]string
+var barCodeMap BarcodeConfig
 
 func init() {
 	var config BarcodeConfig
 	if err := yaml.Unmarshal([]byte(barcodeConfig), &config); err != nil {
 		panic(err)
 	}
-	barCodeMap = config.Codes
+	barCodeMap = config
 }
 
-func main() {
+func newApp() *fiber.App {
 	app := fiber.New()
+	app.Use(logger.New())
 
-	app.Get("/code/:barcode", func(c fiber.Ctx) error {
-		if url, ok := barCodeMap[c.Params("barcode")]; ok {
-			return c.Redirect().To(url)
+	app.Get("/code/:barcode<len(13)>", func(c fiber.Ctx) error {
+		if match, ok := barCodeMap[c.Params("barcode")]; ok {
+			log.Infof("Successful barcode match. %s -> %s", c.Params("barcode"), match)
+			switch match.Kind {
+			case "walmart":
+				return c.Redirect().To(fmt.Sprintf("https://walmart.com/ip/%s", match.Id))
+			default:
+				c.Response().SetStatusCode(fiber.StatusUnprocessableEntity)
+				return c.SendString("Unknown kind: " + match.Kind)
+			}
 		}
 
+		log.Warnf("No barcode match for %s", c.Params("barcode"))
 		return c.SendStatus(fiber.StatusNotFound)
 	})
 
-	log.Fatal(app.Listen(":3500"))
+	return app
+}
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3500"
+	}
+
+	app := newApp()
+
+	if err := app.Listen(":" + port); err != nil {
+		log.Fatal(err)
+	}
 }
